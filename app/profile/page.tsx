a2b2792ee0cadf96bed506a1713c4d9e5e6eb4a7
RaJharit77 <hei.rajo.2@gmail.com>
2024-12-01 22:58:31 +0300

    feat: add a new feature and updating all pages

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -15,75 +15,79 @@ const ProfilePage = () => {
 
     return (
         <div className="flex justify-center items-center h-screen bg-cover bg-center text-white relative" style={{ backgroundImage: 'url(/img/bgProfile.jpg)' }}>
-            {/* Overlay sombre */}
             <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-
-            <div className="relative z-10 bg-gray-900 bg-opacity-60 p-8 rounded-lg shadow-lg w-96 text-menthe bottom-20">
-                <h2 className="text-xl font-bold text-center mb-6">Mon Profil</h2>
+            <div className="relative z-10 bg-gray-900 bg-opacity-60 p-8 rounded-lg shadow-lg w-[40rem] bottom-12">
+                <h2 className="text-2xl font-bold text-center mb-6 text-white">Mon Profil</h2>
                 {user ? (
-                    <>
-                        <div className="mb-4">
-                            <label htmlFor="name" className="block mb-2">Nom</label>
-                            <input
-                                type="text"
-                                id="name"
-                                value={user.name}
-                                readOnly
-                                className="w-full p-2 rounded bg-gray-800 text-white"
-                            />
+                    <div className="space-y-6">
+                        <div className="flex">
+                            <div className="w-1/2 pr-4">
+                                <label htmlFor="name" className="block mb-2 text-white">Nom</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    value={user.name}
+                                    readOnly
+                                    className="w-full p-2 rounded bg-gray-800 text-white"
+                                />
+                            </div>
+                            <div className="w-1/2 pl-4">
+                                <label htmlFor="email" className="block mb-2 text-white">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    value={user.email}
+                                    readOnly
+                                    className="w-full p-2 rounded bg-gray-800 text-white"
+                                />
+                            </div>
                         </div>
-                        <div className="mb-4">
-                            <label htmlFor="email" className="block mb-2">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                value={user.email}
-                                readOnly
-                                className="w-full p-2 rounded bg-gray-800 text-white"
-                            />
+                        <div className="flex">
+                            <div className="w-1/2 pr-4">
+                                <label htmlFor="dob" className="block mb-2 text-white">Date de naissance</label>
+                                <input
+                                    type="text"
+                                    id="dob"
+                                    value={user.dob}
+                                    readOnly
+                                    className="w-full p-2 rounded bg-gray-800 text-white"
+                                />
+                            </div>
+                            <div className="w-1/2 pl-4">
+                                <label htmlFor="birthCity" className="block mb-2 text-white">Ville de naissance</label>
+                                <input
+                                    type="text"
+                                    id="birthCity"
+                                    value={user.birthCity}
+                                    readOnly
+                                    className="w-full p-2 rounded bg-gray-800 text-white"
+                                />
+                            </div>
                         </div>
-                        <div className="mb-4">
-                            <label htmlFor="dob" className="block mb-2">Date de naissance</label>
-                            <input
-                                type="text"
-                                id="dob"
-                                value={user.dob}
-                                readOnly
-                                className="w-full p-2 rounded bg-gray-800 text-white"
-                            />
+                        <div className="flex">
+                            <div className="w-1/2 pr-4">
+                                <label htmlFor="postalCode" className="block mb-2 text-white">Code Postal</label>
+                                <input
+                                    type="text"
+                                    id="postalCode"
+                                    value={user.postalCode}
+                                    readOnly
+                                    className="w-full p-2 rounded bg-gray-800 text-white"
+                                />
+                            </div>
+                            <div className="w-1/2 pl-4">
+                                <label htmlFor="gender" className="block mb-2 text-white">Sexe</label>
+                                <input
+                                    type="text"
+                                    id="gender"
+                                    value={user.gender}
+                                    readOnly
+                                    className="w-full p-2 rounded bg-gray-800 text-white"
+                                />
+                            </div>
                         </div>
-                        <div className="mb-4">
-                            <label htmlFor="birthCity" className="block mb-2">Ville de naissance</label>
-                            <input
-                                type="text"
-                                id="birthCity"
-                                value={user.birthCity}
-                                readOnly
-                                className="w-full p-2 rounded bg-gray-800 text-white"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label htmlFor="postalCode" className="block mb-2">Code Postal</label>
-                            <input
-                                type="text"
-                                id="postalCode"
-                                value={user.postalCode}
-                                readOnly
-                                className="w-full p-2 rounded bg-gray-800 text-white"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label htmlFor="gender" className="block mb-2">Sexe</label>
-                            <input
-                                type="text"
-                                id="gender"
-                                value={user.gender}
-                                readOnly
-                                className="w-full p-2 rounded bg-gray-800 text-white"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label htmlFor="country" className="block mb-2">Pays</label>
+                        <div className="w-1/2 pr-4">
+                            <label htmlFor="country" className="block mb-2 text-white">Pays</label>
                             <input
                                 type="text"
                                 id="country"
@@ -93,13 +97,13 @@ const ProfilePage = () => {
                             />
                         </div>
                         <Link href="/profile/update">
-                            <button className="bg-bleuDiamant text-white hover:bg-bleuTurquoise hover:text-black w-full p-2 rounded mt-4">
+                            <button className="bg-bleuDiamant text-white hover:bg-bleuTurquoise hover:text-black w-full p-2 rounded mt-7">
                                 Mettre à jour mes informations
                             </button>
                         </Link>
-                    </>
+                    </div>
                 ) : (
-                    <p>Utilisateur non trouvé</p>
+                    <p className="text-center text-white">Utilisateur non trouvé</p>
                 )}
             </div>
         </div>
