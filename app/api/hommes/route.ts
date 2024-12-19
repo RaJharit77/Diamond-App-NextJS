42e2028b1f8dbbbf8af595ac7efce009af5b1892
RaJharit77 <hei.rajo.2@gmail.com>
2024-12-20 00:18:42 +0300

    chore: Updating api

--- app/api/hommes/route.ts
+++ app/api/hommes/route.ts
@@ -5,8 +5,8 @@ export async function GET() {
         { id: 1, name: "Hummel sûrvêtement Coton", price: "90€", image: "/img/homme_1.jpg" },
         { id: 2, name: "Hummel blouse sportif Poly", price: "56€", image: "/img/homme_2.jpg" },
         { id: 3, name: "Danemark sweat blue travel", price: "79€", image: "/img/homme_3.jpg" },
-        { id: 4, name: "Danemark Maillot Noir Pro d'Entraînement CDM 2022", price: "129€", image: "/img/homme_4.jpg" },
-        { id: 5, name: "Maillot Everton Extérieur 2023-2024 taille (L, XL, XXL)", price: "110.77€", image: "/img/homme_5.jpg" },
+        { id: 4, name: "Danemark Maillot Noir Pro d'Entraînement CDM 2022", price: "29.99€", image: "/img/homme_4.jpg" },
+        { id: 5, name: "Maillot Everton Extérieur 2023-2024 taille (L, XL, XXL)", price: "30.77€", image: "/img/homme_5.jpg" },
         { id: 6, name: "Hummel T-shirt classique à chevron rouge", price: "45€", image: "/img/homme_6.jpg" },
         { id: 7, name: "Hummel Maillot Denmark Qualification Euro 2024 avec Christian Eriksen", price: "49€", image: "/img/homme_7.jpg" },
         { id: 8, name: "Hummel HMLLGC DEVOTION World Wide SportGear - T-shirt print black", price: "29€", image: "/img/homme_8.jpg" },
@@ -20,6 +20,9 @@ export async function GET() {
         { id: 16, name: "Hummel Virgil T-Shirt Noir L/S/XL/M", price: "80€", image: "/img/homme_16.jpg" },
         { id: 17, name: "Hummel CORE Kit Black & Yellow Sport", price: "73€", image: "/img/homme_17.jpg" },
         { id: 18, name: "Hummel Denmark away Kit 2016-17 released", price: "60€", image: "/img/homme_18.jpg" },
+        { id: 19, name: "Hummel Everton Kit Third Shirt gray", price: "75.55€", image: "/img/homme_19.jpg" },
+        { id: 20, name: "Hummel HMLISAM 20 T-Shirt print Black", price: "63.77€", image: "/img/homme_20.jpg" },
+        { id: 21, name: "Hummel Denmark Diversity T-Shirt 2024", price: "66.43€", image: "/img/homme_21.jpg" },
     ];
 
     return NextResponse.json(products);
