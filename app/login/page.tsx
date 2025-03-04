706df80b9d66fd2cc590fae607a32cb47c0d39db
RaJharit77 <hei.rajo.2@gmail.com>
2025-03-05 00:59:33 +0300

    feat: Update Sign up page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -100,7 +100,7 @@ const LogInPage = () => {
                     Se connecter
                 </button>
                 <div className="mt-4 text-center">
-                    <Link href="/signUp" className="text-corail hover:underline">
+                    <Link href="/signup" className="text-corail hover:underline">
                         Créer un compte
                     </Link>
                 </div>
